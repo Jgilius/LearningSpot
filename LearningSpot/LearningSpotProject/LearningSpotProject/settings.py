f4de4cd651a98425a3601c90706b61ff043da1fe
Jgilius <2203577g@student.gla.ac.uk>
2022-07-30 22:10:30 +0100

    Updates to HTML and CSS

--- LearningSpot/LearningSpotProject/LearningSpotProject/settings.py
+++ LearningSpot/LearningSpotProject/LearningSpotProject/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,9 @@ ROOT_URLCONF = 'LearningSpotProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
