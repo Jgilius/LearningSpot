482bab3fb7a300adab800b751f9bb3abc59d3778
Jgilius <2203577g@student.gla.ac.uk>
2022-08-08 07:46:10 +0100

    created student.html and removed home.html

--- LearningSpot/LearningSpotProject/lsbase/urls.py
+++ LearningSpot/LearningSpotProject/lsbase/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home/', views.home, name="home"),
-
+    path('student/', views.student, name="student")
 ]
--- LearningSpot/LearningSpotProject/lsbase/views.py
+++ LearningSpot/LearningSpotProject/lsbase/views.py
@@ -5,13 +5,10 @@ from .models import learning_task
 def index(request):
     return render(request, 'index.html')
 
-def home(request):
+def student(request):
     learning_intentions = learning_intention.objects.all().order_by('created')
     learning_tasks = learning_task.objects.all().order_by('created')
 
     dynamic_content = {'learning_intentions' : learning_intentions, 'learning_tasks' : learning_tasks}
-    return render(request, 'home.html', dynamic_content)
+    return render(request, 'student.html', dynamic_content)
 
-    
-# {'learning_tasks' : learning_tasks}
-#   learning_tasks = learning_task.objects.all().order_by('created')
