825992ceb9f9ae67b3300b4d5e9ec6266b06965f
Jgilius <2203577g@student.gla.ac.uk>
2022-08-11 23:57:06 +0100

    js changes

--- LearningSpot/LearningSpotProject/static/JS/learning_intentions.js
+++ LearningSpot/LearningSpotProject/static/JS/learning_intentions.js
@@ -1,11 +1,13 @@
 
-//  const happy_button_select = document.querySelector(".hs").value;
-//  const unsure_button_select = document.querySelector(".us").value;
-//  const sad_button_select = document.querySelector(".ss").value;
-//  const happy_button_unselect = document.querySelector(".hu").value;
-//  const unsure_button_unselect = document.querySelector(".uu").value;
-//  const sad_button_unselect = document.querySelector(".su").value;
+ const happy_button_select = document.getElementById("hs").value;
+ const unsure_button_select = document.getElementById("us").value;
+ const sad_button_select = document.getElementById("ss").value;
+ const happy_button_unselect = document.getElementById("hu").value;
+ const unsure_button_unselect = document.getElementById("uu").value;
+ const sad_button_unselect = document.getElementById("su").value;
+ const happy_form = document.getElementById("happy_form").value;
 
+ let happy_clicked;
 
  function all_active(){
      hs.disabled = false; 
@@ -13,10 +15,11 @@
      ss.disabled = false; 
  }
 
+
  function happy_onclick(){
 
-    us.disabled = true;
-    ss.disabled = true;
+        us.disabled = true;
+        ss.disabled = true;
  }
 
  function unsure_onclick(){
