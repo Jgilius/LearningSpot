2f3b474c7d1b64eb36b6f8a03f795814ad96487d
Jgilius <2203577g@student.gla.ac.uk>
2022-08-27 18:45:15 +0100

    Agora App Set-up

--- LearningSpot/LearningSpotProject/LearningSpotProject/settings.py
+++ LearningSpot/LearningSpotProject/LearningSpotProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lsbase.apps.BaseConfig',
+    'agora',
 ]
 
 MIDDLEWARE = [
--- LearningSpot/LearningSpotProject/LearningSpotProject/urls.py
+++ LearningSpot/LearningSpotProject/LearningSpotProject/urls.py
@@ -5,6 +5,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('', include('lsbase.urls')),
+    path('', include('agora.urls')),
+    path('admin/', admin.site.urls),
 ]
